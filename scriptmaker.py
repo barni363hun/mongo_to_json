59072e0f3cf0dab1f5cf54884c27a0e8767daba4
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-14 19:51:49 +0100

    projections added + ans

--- scriptmaker.py
+++ scriptmaker.py
@@ -1,8 +1,9 @@
 import json
 input_json = open("./query.json", "r")
 input_json_object = json.loads(input_json.read())
-script = input_json_object["script"].replace(" ", "").replace("\n", "")
-output = script.replace(
-    "#", str(input_json_object["query"]).replace("\n", ""))
+output = input_json_object["script"].replace(" ", "").replace("\n", "")
+output = output.replace("#", str(input_json_object["query"]).replace("\n", ""))
+output = output.replace(
+    "@", str(input_json_object["projections"]).replace("\n", ""))
 open("script.txt", "w").write(output)
 input_json.close()
