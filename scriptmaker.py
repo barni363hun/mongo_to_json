6e88918ecba174bee5d4167a16090948a38cf9bd
Princzes Barnabás <barni.pbs@gmail.com>
2021-11-14 18:32:31 +0100

    removed space replace, ans

--- scriptmaker.py
+++ scriptmaker.py
@@ -3,6 +3,6 @@ input_json = open("./query.json", "r")
 input_json_object = json.loads(input_json.read())
 script = input_json_object["script"].replace(" ", "").replace("\n", "")
 output = script.replace(
-    "#", str(input_json_object["query"]).replace(" ", "").replace("\n", ""))
+    "#", str(input_json_object["query"]).replace("\n", ""))
 open("script.txt", "w").write(output)
 input_json.close()
